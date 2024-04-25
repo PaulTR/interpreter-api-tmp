454b35c5992241a6e221129a1b2bdf18194a7caf
st-haingo <hai.ngo@supremetech.vn>
2024-04-25 15:40:39 +0700

    feat: classify using mobile bert and average word vector

--- text_classification/android/app/build.gradle.kts
+++ text_classification/android/app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.textclassification"
+    namespace = "com.google.edgeai.examples.text_classification"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.textclassification"
+        applicationId = "com.google.edgeai.examples.text_classification"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -60,14 +60,15 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.tensorflow.lite)
-    implementation(libs.tensorflow.lite.metadata)
     implementation(libs.tensorflow.lite.support)
-    implementation(libs.tensorflow.lite.text)
+    implementation(libs.tensorflow.lite.metadata)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
